1d1f7cfae8a1b76c7d0392f7b62b0c3a6abcaf3a
Ayush Ranjan <ayush748830@gmail.com>
2026-02-03 17:19:00 +0530

    Used external libraries to fix the checkout date

--- data/cart.js
+++ data/cart.js
@@ -4,9 +4,11 @@ if (!cart) {
   cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
+  deliveryOptionId: '1'
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '2'
 }];
 }
 
@@ -29,6 +31,7 @@ export function addToCart(productId) { //We need to give productId as parameter 
         //the cart array already exists and we are pushing the product name and its quanity as objects into that cart when the button is clicked
         productId: productId,
         quantity: 1,
+        deliveryOptionId: '1'
       });
     }
     saveToStorage();  
