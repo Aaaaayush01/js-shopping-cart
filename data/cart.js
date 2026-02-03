1a5191fba04947ba6404537cd519711537b40cb4
Ayush Ranjan <ayush748830@gmail.com>
2026-02-03 11:41:05 +0530

    Added the shopping cart and checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,48 @@
-const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+},{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) { //We need to give productId as parameter and then as argument while calling this func because the function is using it
+  let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        //the cart array already exists and we are pushing the product name and its quanity as objects into that cart when the button is clicked
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    saveToStorage();  
+}
+
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  saveToStorage();
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,10 +1,14 @@
 //Deleted the manually created products array to load the already made array in data folder
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-let productsHtml = '';
+let productsHtml = "";
 
-products.forEach((product) => { //doing stars * 10 to match image file name (4.5 * 10)
-    // Accumulator Pattern
-    productsHtml += `
+products.forEach((product) => {
+  //doing stars * 10 to match image file name (4.5 * 10)
+  // Accumulator Pattern
+  productsHtml += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -24,7 +28,7 @@ products.forEach((product) => { //doing stars * 10 to match image file name (4.5
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -49,16 +53,27 @@ products.forEach((product) => { //doing stars * 10 to match image file name (4.5
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id ="${product.id}">
             Add to Cart
           </button>
         </div>
-    `
+    `;
 });
-document.querySelector('.js-products-grid').innerHTML = productsHtml;
+document.querySelector(".js-products-grid").innerHTML = productsHtml;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log('Product Added.');
+function updateCartQuantity() {
+  let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
     });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
+  });
 });
