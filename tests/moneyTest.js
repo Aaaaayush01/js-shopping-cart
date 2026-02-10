e97f81b724c6fdf322e559656fd9880cf8f1093b
Ayush Ranjan <ayush748830@gmail.com>
2026-02-10 15:10:17 +0530

    Remove default test cases and added money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -4,4 +4,16 @@ if (formatCurrency(2095) === '20.95') {
     console.log('passed');
 } else {
     console.log('failed');
+};
+
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+};
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('passed');
+} else {
+    console.log('failed');
 }
